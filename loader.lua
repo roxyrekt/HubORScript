a5d16e6e273d524370dd6461c678e77c848c5567
roxyrekt <87309991+roxyrekt@users.noreply.github.com>
2021-08-24 23:15:55 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local a = game.PlaceId
+
+if a == 5543622168 then -- Place ID / Game ID
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/roxyrekt/HubORScript/main/ctdOP.lua"))() --Loadstring
+
+                        elseif a == 2017674707 then -- Place ID / Game ID
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/roxyrekt/HubORScript/main/youtubelifesim.lua"))() --Loadstring
+
+                        else game.Players.LocalPlayer:Kick("The game is not supported by this script")
+end
